10a4bf0bd59ebd6c62b760d9d01a243a42bb6bc4
Rastanislav <morfij@gmail.com>
2015-03-06 15:09:43 +0100

    Update makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -17,12 +17,6 @@ echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-mkdir $dir
-cd $dir
-echo "...done"
-
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
