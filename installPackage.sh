976a0422227932641298a17e4d5c01b5569bbfe5
Rastanislav <morfij@gmail.com>
2016-10-22 18:35:25 +0200

    Update installPackage.sh

--- installPackage.sh
+++ installPackage.sh
@@ -20,6 +20,7 @@ echo "Installing packages"
 	sudo apt-get install --yes phpmyadmin 
 	sudo apt-get install --yes php-xml
 	sudo apt-get install --yes php5.6-xml
+	sudo apt-get install --yes php7.0-mcrypt  
 	sudo apt-get install --yes apache2-utils
 	pip3 install mps-youtube 
 	sudo apt-get install --yes zsh
