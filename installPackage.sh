554153782d86a8d6f49ca721cafbdcdf68545a98
Rastanislav <morfij@gmail.com>
2022-08-15 13:44:33 +0300

    Update installPackage.sh

--- installPackage.sh
+++ installPackage.sh
@@ -118,4 +118,12 @@ sudo apt-get install pritunl-client-electron
 	echo "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang-22.x" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
 	sudo apt install --yes rabbitmq-server
 	sudo systemctl start mongod
+	sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb https://repo.pritunl.com/stable/apt jammy main
+EOF
+
+sudo apt --assume-yes install gnupg
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
+sudo apt update
 echo "Done installing packages"
